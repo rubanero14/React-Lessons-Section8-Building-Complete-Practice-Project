66b8be570f70896688bb4c591a9fa1b45b6ef7a7
Rajaruban Rajindram <ahyat888@gmail.com>
2022-12-25 19:34:26 +0800

    added form validation messages on DOM

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,6 +7,8 @@ import styles from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [inputValid, setInputValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
   // onChange event handlers
   const userNameChangeHandler = (e) => {
@@ -18,14 +20,22 @@ const AddUser = (props) => {
 
   // onSubmit event handler =>
   const addUserHandler = (event) => {
+    // Resetting validation
+    setInputValid(false);
+    setErrorMessage("");
+
     // Prevent page from refreshing upon submit evnt fired
     event.preventDefault();
 
     // Gatekeeping function to bar submitting empty data
-    if (enteredUserName === "" || enteredAge === "") {
-      return console.log("Fill up up the inputs");
-    } else if (parseInt(enteredAge) < 1) {
-      return console.log("Invalid age");
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setInputValid(false);
+      setErrorMessage("Fill up up the inputs!");
+      return console.log(inputValid, errorMessage);
+    } else if (+enteredAge < 1) {
+      setInputValid(false);
+      setErrorMessage("Invalid age!");
+      return console.log(inputValid, errorMessage);
     }
 
     // Storing user input data into an object
@@ -62,7 +72,15 @@ const AddUser = (props) => {
           onChange={ageChangeHandler}
           value={enteredAge}
         />
-        <Button type="submit">Add User</Button>
+        <Button type="submit" style={{ marginBotttom: "10px" }}>
+          Add User
+        </Button>
+        <p
+          className={styles.alert}
+          style={{ display: inputValid ? "none" : "block" }}
+        >
+          {errorMessage}
+        </p>
       </form>
     </Card>
   );
